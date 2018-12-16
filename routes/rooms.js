10011c70acbc702b24dca06af046522609375ee4
Presenter <hendrik.wermser@ironhack.com>
2018-12-16 17:05:17 +0100

    adds roomsLayout that includes the JS needed only for rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,23 +4,23 @@ const { isConnected } = require('../middlewares')
 const router = express.Router();
 
 router.get('/1', isConnected, (req, res, next) => {
-  res.render('rooms/1');
+  res.render('rooms/1', { layout: 'roomsLayout' });
 });
 
 router.get('/2', isConnected, (req, res, next) => {
-  res.render('rooms/2');
+  res.render('rooms/2', { layout: 'roomsLayout' });
 });
 
 router.get('/chocolate', isConnected, (req, res, next) => {
-  res.render('rooms/chocolate');
+  res.render('rooms/chocolate', { layout: 'roomsLayout' });
 });
 
 router.post('/chocolate', isConnected, (req, res, next) => {
   let { color } = req.body
   if (color !== '7B3F00')
-    res.render('rooms/chocolate');
+    res.render('rooms/chocolate', { layout: 'roomsLayout' });
   else {
-    res.redirect('/rooms/wip')
+    res.redirect('/rooms/wip', { layout: 'roomsLayout' });
   }
 });
 
