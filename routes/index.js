9473586db39676ee047b7829336d73c19a32e0ae
Presenter <hendrik.wermser@ironhack.com>
2018-12-16 15:59:34 +0100

    fully working chat implementation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Team = require('../models/Team.js')
+const { isConnected } = require('../middlewares')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/chat', isConnected, (req, res, next) => {
+  Team.findById(req.user.id).then((doc) => {
+    res.render('chat', { layout: false, messages: doc.messages, teamname: doc.name });
+  })
+})
+
+router.post('/chat', isConnected, (req, res, next) => {
+  Team.findByIdAndUpdate(req.user.id, { $push: { messages: { isFromTeam: true, text: req.body.input } } }, { new: true }).then((doc) => {
+    res.render('chat', { layout: false, messages: doc.messages, teamname: doc.name });
+  })
+})
 
 module.exports = router;
