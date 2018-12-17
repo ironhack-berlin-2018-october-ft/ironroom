7410a702c2d7d0bf1cc874445bcae281c9fcc3d5
tomjohnburton <tomjohnburton@gmail.com>
2018-12-17 17:43:10 +0100

    newroom

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,54 +1,69 @@
-const express = require('express')
-const { isConnected, redirectToNextRoom } = require('../middlewares')
-const rooms = require('../data/rooms')
+const express = require("express");
+const { isConnected, redirectToNextRoom } = require("../middlewares");
+const rooms = require("../data/rooms");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.redirect(rooms[req.user.roomIndex].url)
-})
+router.get("/", (req, res, next) => {
+  res.redirect(rooms[req.user.roomIndex].url);
+});
 
-router.get('/0', (req, res, next) => {
-  res.render('rooms/1')
-})
+router.get("/0", (req, res, next) => {
+  res.render("rooms/1");
+});
 
-router.get('/1', (req, res, next) => {
-  res.render('rooms/1')
-})
+router.get("/1", (req, res, next) => {
+  res.render("rooms/1");
+});
 
-router.get('/2', (req, res, next) => {
-  res.render('rooms/2')
-})
+router.get("/2", (req, res, next) => {
+  res.render("rooms/2");
+});
 
-router.get('/gadget', (req, res, next) => {
-  res.render('rooms/gadget')
-})
+router.get("/gadget", (req, res, next) => {
+  res.render("rooms/gadget");
+});
 
-router.get('/chartreuse', (req, res, next) => {
-  res.render('rooms/chartreuse')
-})
+router.get("/chartreuse", (req, res, next) => {
+  res.render("rooms/chartreuse");
+});
 
-router.post('/chartreuse', (req, res, next) => {
-  let color = (req.body.color + "").toLowerCase()
-  if (color !== '7fff00')
-    res.render('rooms/chartreuse')
-  else {
-    redirectToNextRoom(req, res, next)
+router.get("/bridgeoftruth", (req, res, next) => {
+  res.render("rooms/bridgeoftruth");
+});
+router.get("/whatadrag", (req, res, next) => {
+  res.render("rooms/whatadrag");
+});
+
+router.post("/bridgeoftruth", (req, res, next) => {
+  let answer = "==";
+  let { truth } = req.body;
+  if (answer == truth) {
+    // redirectToNextRoom(req, res, next);
+    res.render("success");
+  } else {
+    res.render("rooms/bridgeoftruth");
   }
-})
+});
 
-router.get('/hex-color', (req, res, next) => {
-  res.render('rooms/hex-color')
-})
-
-router.post('/hex-color', (req, res, next) => {
-  let fruit = (req.body.fruit + "").toLowerCase()
-  if (fruit !== 'banana')
-    res.render('rooms/hex-color')
+router.post("/chartreuse", (req, res, next) => {
+  let color = (req.body.color + "").toLowerCase();
+  if (color !== "7fff00") res.render("rooms/chartreuse");
   else {
-    redirectToNextRoom(req, res, next)
+    redirectToNextRoom(req, res, next);
   }
-})
+});
 
+router.get("/hex-color", (req, res, next) => {
+  res.render("rooms/hex-color");
+});
 
-module.exports = router
+router.post("/hex-color", (req, res, next) => {
+  let fruit = (req.body.fruit + "").toLowerCase();
+  if (fruit !== "banana") res.render("rooms/hex-color");
+  else {
+    redirectToNextRoom(req, res, next);
+  }
+});
+
+module.exports = router;
