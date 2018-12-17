47d88918f02bee405953980b2701076c0c2c1e6f
carloscadel <c.cazallas.delfa@gmail.com>
2018-12-17 15:35:17 +0100

    add consoling room

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,41 +1,54 @@
-const express = require('express')
-const { isConnected, redirectToNextRoom } = require('../middlewares')
-const rooms = require('../data/rooms')
+const express = require("express");
+const { isConnected, redirectToNextRoom } = require("../middlewares");
+const rooms = require("../data/rooms");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.redirect(rooms[req.user.roomIndex].url)
-})
+router.get("/", (req, res, next) => {
+  res.redirect(rooms[req.user.roomIndex].url);
+});
 
-router.get('/0', (req, res, next) => {
-  res.render('rooms/1')
-})
+router.get("/0", (req, res, next) => {
+  res.render("rooms/1");
+});
 
-router.get('/1', (req, res, next) => {
-  res.render('rooms/1')
-})
+router.get("/1", (req, res, next) => {
+  res.render("rooms/1");
+});
 
-router.get('/2', (req, res, next) => {
-  res.render('rooms/2')
-})
+router.get("/2", (req, res, next) => {
+  res.render("rooms/2");
+});
 
-router.get('/gadget', (req, res, next) => {
-  res.render('rooms/gadget')
-})
+router.get("/gadget", (req, res, next) => {
+  res.render("rooms/gadget");
+});
 
-router.get('/chartreuse', (req, res, next) => {
-  res.render('rooms/chartreuse')
-})
+router.get("/chartreuse", (req, res, next) => {
+  res.render("rooms/chartreuse");
+});
 
-router.post('/chartreuse', (req, res, next) => {
-  let color = (req.body.color + "").toLowerCase()
-  if (color !== '7fff00')
-    res.render('rooms/chartreuse')
+router.post("/chartreuse", (req, res, next) => {
+  let color = (req.body.color + "").toLowerCase();
+  if (color !== "7fff00") res.render("rooms/chartreuse");
   else {
-    redirectToNextRoom(req, res, next)
+    redirectToNextRoom(req, res, next);
   }
-})
+});
 
+router.get("/consoling", (req, res, next) => {
+  res.render("rooms/consoling");
+});
 
-module.exports = router
+router.post("/consoling", (req, res, next) => {
+  if (
+    req.body.console1 === "checked" &&
+    req.body.console3 === "checked" &&
+    req.body.console7 === "checked"
+  ) {
+    redirectToNextRoom(req, res, next);
+  }
+  res.render("rooms/consoling");
+});
+
+module.exports = router;
